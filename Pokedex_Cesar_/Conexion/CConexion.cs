01a469b4a207e83db55588295363c4ef8f2d6eab
Cesar-Andres-R <bocchi.the.rock.010@gmail.com>
2023-11-28 07:52:52 -0700

    Pokedex_Cesar

--- Pokedex_Cesar_/Conexion/CConexion.cs
+++ Pokedex_Cesar_/Conexion/CConexion.cs
@@ -7,6 +7,8 @@ namespace Pokedex_Cesar_.Conexion
 {
     public class CConexion
     {
+        //Esta es la base de datos en tiempo real, y el link es tu base de datos no relacional
+        //que estoy utilizando.
         public static FirebaseClient firebase = new FirebaseClient("https://mvvm-pokedex-e7d60-default-rtdb.firebaseio.com/");
     }
 }
